fffda772608e89e147d6af8abf981e7842180dd4
alexwoolford <alex@woolford.io>
2026-03-08 00:29:48 -0700

    domain_status 0.1.14

--- Formula/domain_status.rb
+++ Formula/domain_status.rb
@@ -7,16 +7,16 @@ class DomainStatus < Formula
   on_macos do
     if Hardware::CPU.arm?
       url "https://github.com/alexwoolford/domain_status/releases/download/v0.1.14/domain_status-macos-aarch64.tar.gz"
-      sha256 "0000000000000000000000000000000000000000000000000000000000000000"
+      sha256 "9297c29971334ec13a7a08b08c2da895b610cef06c41dc7ef7a5186d585d229b"
     else
       url "https://github.com/alexwoolford/domain_status/releases/download/v0.1.14/domain_status-macos-x86_64.tar.gz"
-      sha256 "0000000000000000000000000000000000000000000000000000000000000000"
+      sha256 "cafb452c1469207abf1b72e376c400d100decda67c152b5e665762118b98916a"
     end
   end
 
   on_linux do
     url "https://github.com/alexwoolford/domain_status/releases/download/v0.1.14/domain_status-linux-x86_64.tar.gz"
-    sha256 "0000000000000000000000000000000000000000000000000000000000000000"
+    sha256 "2a1f717d3911729fc9fa4884d83411bac3864f8b10cb8f557a1bba4fad43ffe5"
   end
 
   def install
