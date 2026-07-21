36bc212c77ef4e98cb7359a363be3ed4cca1810e
Alex Woolford <alex@woolford.io>
2026-07-21 16:28:20 -0600

    Update domain_status to v0.1.22

--- Formula/domain_status.rb
+++ Formula/domain_status.rb
@@ -1,22 +1,22 @@
 class DomainStatus < Formula
   desc "Concurrent URL status checker that captures comprehensive metadata in SQLite"
   homepage "https://github.com/alexwoolford/domain_status"
-  version "0.1.21"
+  version "0.1.22"
   license "MIT"
 
   on_macos do
     if Hardware::CPU.arm?
-      url "https://github.com/alexwoolford/domain_status/releases/download/v0.1.21/domain_status-macos-aarch64.tar.gz"
-      sha256 "009ca0427da84ea301e168b9fcabae43fa260301435745f38c8e5f284e10afdb"
+      url "https://github.com/alexwoolford/domain_status/releases/download/v0.1.22/domain_status-macos-aarch64.tar.gz"
+      sha256 "f9e5cfc24739e042f29ba062eeccc28f41127629e599bd0642cdd4ba55c6fd22"
     else
-      url "https://github.com/alexwoolford/domain_status/releases/download/v0.1.21/domain_status-macos-x86_64.tar.gz"
-      sha256 "7b7c096c4dbf914c2bdf0e0f40edbf10cf0286bed83b86f7023d6bec17f3d54b"
+      url "https://github.com/alexwoolford/domain_status/releases/download/v0.1.22/domain_status-macos-x86_64.tar.gz"
+      sha256 "0a06cb4e342191974267edc752c59731730f1d591fdb5a04d506bf7e232c0108"
     end
   end
 
   on_linux do
-    url "https://github.com/alexwoolford/domain_status/releases/download/v0.1.21/domain_status-linux-x86_64.tar.gz"
-    sha256 "606a906cf43d73594aac0fa5450a9b9496e26f462dd731f9d1ecfcb3094427b3"
+    url "https://github.com/alexwoolford/domain_status/releases/download/v0.1.22/domain_status-linux-x86_64.tar.gz"
+    sha256 "7f53d3633e950c2695fe6af26d4b7e58deedd867b7c3aa2bcc0d7635b68f30ea"
   end
 
   def install
@@ -24,6 +24,6 @@ class DomainStatus < Formula
   end
 
   test do
-    assert_match "domain_status", shell_output("\#{bin}/domain_status --version")
+    assert_match "domain_status", shell_output("#{bin}/domain_status --version")
   end
 end
